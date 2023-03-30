a0093e26660e1b9f2c831ed1a28774f3598b0a03
Vladyslav Khibovskyi <vlad.cv.2000@gmail.com>
2023-03-30 20:51:48 +0200

    Edit 2fa view

--- resources/views/profile/partials/two-factor-auth-form.blade.php
+++ resources/views/profile/partials/two-factor-auth-form.blade.php
@@ -1,20 +1,36 @@
 <section class="space-y-6">
     <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Two-Factor Authentication (2FA)') }}
-        </h2>
-
+               
+        <div class="flex items-center justify-start mt-4">
+            <h2 class="text-lg font-medium text-gray-900">
+                {{ __('Two-Factor Authentication (2FA)') }}
+            </h2>
+            @if ($user->name === 'vladislav_0372')
+                <h2 class="text-lg font-medium text-red-600">
+                    &nbsp;
+                    {{ __('Disabled') }}
+                </h2>
+            @else
+                <h2 class="text-lg font-medium text-green-600">
+                    &nbsp;
+                    {{ __('Enabled') }}
+                </h2>
+            @endif           
+        </div>
         <p class="mt-1 text-sm text-gray-600">
-            {{ __('Two-Factor Authentication (aka 2FA) is a specific type of Multi-Factor Authentication that requires the authenticating party to produce two separate identifying factors. that are indicative to its identity, instead of the previously standard single identifier, usually a password, required in many systems.') }}
+            {{ __('Two-Factor Authentication (aka 2FA) is a specific type of Multi-Factor Authentication that requires the authenticating party to produce two separate identifying factors that are indicative to its identity, instead of the previously standard single identifier, usually a password, required in many systems.') }}
         </p>
+        
     </header>
-
-    <!-- <x-info-button
-        x-data=""
-    >{{ __('Enable 2FA') }}</x-info-button> -->
-    <x-primary-button
-        x-data=""
-    >{{ __('Enable 2FA') }}</x-primary-button>
-
+    
+    @if ($user->name === 'vladislav_0372')
+        <x-green-button x-data="">
+            {{ __('Enable') }}
+        </x-green-button> 
+    @else
+        <x-danger-button x-data="">
+            {{ __('Disable') }}
+        </x-danger-button>
+    @endif
 
 </section>
