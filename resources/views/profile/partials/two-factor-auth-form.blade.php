60f4ecd57e7269e2d17747f9cdb278b151c65649
Vladyslav <vlad.cv.2000@gmail.com>
2023-03-30 15:34:40 +0200

    Create Laravel/Blade default project

---
+++ resources/views/profile/partials/two-factor-auth-form.blade.php
@@ -0,0 +1,20 @@
+<section class="space-y-6">
+    <header>
+        <h2 class="text-lg font-medium text-gray-900">
+            {{ __('Two-Factor Authentication (2FA)') }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600">
+            {{ __('Two-Factor Authentication (aka 2FA) is a specific type of Multi-Factor Authentication that requires the authenticating party to produce two separate identifying factors. that are indicative to its identity, instead of the previously standard single identifier, usually a password, required in many systems.') }}
+        </p>
+    </header>
+
+    <!-- <x-info-button
+        x-data=""
+    >{{ __('Enable 2FA') }}</x-info-button> -->
+    <x-primary-button
+        x-data=""
+    >{{ __('Enable 2FA') }}</x-primary-button>
+
+
+</section>
