74c4aa60c6b05a893822578ebeec14f727a23646
Vladyslav <vlad.cv.2000@gmail.com>
2023-04-19 14:43:58 +0200

    Edit config file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,8 +41,7 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-$app->register(\Barryvdh\DomPDF\ServiceProvider::class);
-$app->configure('dompdf');
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
