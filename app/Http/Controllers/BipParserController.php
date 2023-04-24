f75b193b32f1b840be60c513f60bb8f3f7831cc6
Vladyslav Khibovskyi <vlad.cv.2000@gmail.com>
2023-04-25 01:26:07 +0200

    Update Parse method

--- app/Http/Controllers/BipParserController.php
+++ app/Http/Controllers/BipParserController.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Session;
 
 class BipParserController extends Controller
 {
+    private const REWRITE_TXT = true;
+    //private const SEARCH_ENGINE = 'google';
+    private const SEARCH_ENGINE = 'bing';
     public function index(Request $request): View
     {
         return view('bip-parser.index');
@@ -19,7 +22,7 @@ class BipParserController extends Controller
 
     public function parse1(Request $request): View
     {
-        
+
         $url = "/url?q=https://www.bip.ires.pl/sedziszow_mlp/drukuj_6426__127064__/&sa=U&ved=2ahUKEwj8nqrdqsP-AhWI7jgGHXnGDMg4ChAWegQICRAC&usg=AOvVaw1S8-ooaRWw_I7vO0z8X-4s";
         //$parts = parse_url($url);
         //Log::info($parts);
@@ -29,24 +32,67 @@ class BipParserController extends Controller
 
         Log::info($whatINeed);
 
+        $myfile = fopen("txt_files/scrapedLinks.txt", "w") or die("Unable to open file!");
+        $txt = "John Doe\n";
+        fwrite($myfile, $txt);
+        $txt = "Jane Doe\n";
+        fwrite($myfile, $txt);
+        fclose($myfile);
+
         return view('bip-parser.index');
-                                            
+
     }
     public function parse(Request $request): View
     {
-        //$request->input('action')
-        
-        //$pageStart = 10;
+        if(self::REWRITE_TXT){
+
+            $links = self::scrapeLinks()['links'];
+
+            $txtFile = fopen("txt_files/scrapedLinks.txt", "w") or die("Unable to open file!");
+
+            foreach($links as $link){
+                fwrite($txtFile, $link . "\n");
+            }
+
+            fclose($txtFile);
+        }else{
+            $array = explode("\n", file_get_contents("txt_files/scrapedLinks.txt"));
+            array_pop($array);
+
+            //Log::info($array);
+        }
+
+
+
+        // if($request->input('action') == 'dirtyParse'){
+        //     return view('bip-parser.index')->with('status', 'showLinks')
+        //                                     ->with('links', $fullLinks);
+        // }else{
+        //     return view('bip-parser.index')->with('status', 'showLinks')
+        //                                     ->with('links', $links);
+        // }
+
+        // $trimedLink =  'https://www.bip.ires.pl/czg&sa=U&ved=2ahUKEwif-t6Ns8P-AhVuzDgGHZVWCgoQFnoECAkQAg&usg=AOvVaw1jykwl8Wtue9RnYVV4SxIj';
+        // $govno = explode('&', $trimedLink);
+        // Log::info($govno);
+         return view('bip-parser.index');
+    }
+
+    function scrapeLinks(){
         $pagesNumber = 5;
         $domainName = 'https://www.bip.ires.pl/';
-        
-        //$link = "https://www.bip.ires.pl/sedziszow_mlp";
 
-        for ($i = 0; $i < $pagesNumber * 10; $i+=10) {
-            $url = "https://www.google.pl/search?q=site%3Abip.ires.pl&start=" . $i;
+        if(self::SEARCH_ENGINE == 'bing'){
+            $i = 1;
+            $urlBase = "https://www.bing.com/search?q=site%3Abip.ires.pl&first=";
+        }else{
+            $i = 0;
+            $urlBase = "https://www.google.pl/search?q=site%3Abip.ires.pl&start=";
+        }
+
+        for (; $i < $pagesNumber * 10; $i+=10) {
+            $url = $urlBase . $i;
             $page = file_get_contents($url, false);
-            // //https://www.google.pl/search?q=site%3Abip.ires.pl&start=10
-            
 
             //Log::info($page);
             $doc = new DOMDocument();
@@ -54,46 +100,59 @@ class BipParserController extends Controller
             $doc->loadHTML($page);
             libxml_use_internal_errors(false);
             //Log::info($page);
-            if($request->input('action') == 'dirtyParse'){
-                foreach ($doc->getElementsByTagName('a') as $element) {
-                    if ($element->hasAttribute('href')) {
-                        $link = $element->getAttribute('href');
-                        
-                        $fullLinks[] =  $link;          
+
+            if(self::SEARCH_ENGINE == 'bing'){
+                foreach ($doc->getElementsByTagName('cite') as $element) {
+                    $link = $element->textContent;
+                    $fullLinks[] =  $link;
+
+                    if (str_contains($link, $domainName)) {
+                        $trimedLink = strval(explode('/', $link)[3]);
+
+                        if(!is_numeric($trimedLink)){
+                            $subdomainNames[] =  $trimedLink;
+                            $links[] = $domainName . $trimedLink;
+                        }
                     }
                 }
             }else{
                 foreach ($doc->getElementsByTagName('a') as $element) {
                     if ($element->hasAttribute('href')) {
                         $link = $element->getAttribute('href');
-                                        
+
+                        $fullLinks[] =  $link;
+
                         if (str_contains($link, $domainName)) {
                             $trimedLink = strval(explode('/', $link)[4]);
-                            
+
                             if(str_contains($trimedLink, '&')){
                                 $trimedLink = strval(explode('&', $trimedLink)[0]);
                             }
 
                             if(!is_numeric($trimedLink)){
                                 $subdomainNames[] =  $trimedLink;
-                                $links[] = $domainName . $trimedLink; 
-                            }                                       
-                        }                              
+                                $links[] = $domainName . $trimedLink;
+                            }
+                        }
                     }
-                }
-            } 
+                }        
+            }
+
+            $subdomainNames = array_unique($subdomainNames);
+            $links = array_unique($links);
+
+            $arrayGroup = array(
+                'subdomainNames' => $subdomainNames,
+                'links' => $links,
+                'fullLinks' => $fullLinks,
+            );
         }
 
-        $subdomainNames = array_unique($subdomainNames);
-        $links = array_unique($links);
-        
-        Log::info($links);
-       
-        return view('bip-parser.index')->with('status', 'showLinks')
-                                      ->with('links', $links);
-        // $trimedLink =  'https://www.bip.ires.pl/czg&sa=U&ved=2ahUKEwif-t6Ns8P-AhVuzDgGHZVWCgoQFnoECAkQAg&usg=AOvVaw1jykwl8Wtue9RnYVV4SxIj';
-        // $govno = explode('&', $trimedLink); 
-        // Log::info($govno);                  
-        // return view('bip-parser.index');
+
+
+        Log::info($arrayGroup);
+
+        return $arrayGroup;
+        //Log::info($links);
     }
 }
